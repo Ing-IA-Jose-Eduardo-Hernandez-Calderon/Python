d30429dc02a1fddede83bb92d910918543b37266
Ing. Calderón <pollo110119@gmail.com>
2024-11-27 09:11:24 -0600

    Add files via upload

---
+++ SumaDosNumerosPP.py
@@ -0,0 +1,8 @@
+# Este código realiza la suma de dos números y lo muestra en pantalla, que corresponde a mi segundo proyecto personal
+
+number1 = 25
+number2 = 35
+resultado = 0
+
+resultado = number1 + number2
+print(f"La suma de {number1} + {number2} es: {resultado}")
