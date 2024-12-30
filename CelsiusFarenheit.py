b573365be60adc5d6d8e046433a99345069bbd0f
Ing. Calderón <pollo110119@gmail.com>
2024-12-30 01:14:40 -0600

    Delete CelsiusFarenheit.py

--- CelsiusFarenheit.py
+++
@@ -1,7 +0,0 @@
-# Este código convierte los grados celsius a Farenheit, que corresponde a mi cuarto proyecto personal
-
-Celsius = float(input("Ingresa los grados Celsius que deseas convertir a grados Farenheit: "))
-
-Conversion_Fahrenheit = (Celsius * 9/5) + 32
-print(f"{Celsius} grados Celsius convertidos a grados Fahrenheit son: {Conversion_Fahrenheit}")
-
