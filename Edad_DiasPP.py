58959206d7cd33bcc8f5965ef0e0725ccbbce6e3
Ing. Calderón <pollo110119@gmail.com>
2024-12-31 23:53:57 -0600

    Update Edad_DiasPP.py

--- Edad_DiasPP.py
+++ Edad_DiasPP.py
@@ -7,7 +7,7 @@ edad = int(input("¿Cuántos años tienes?: "))
 dias_año = 365
 
 #Calculo de la edad del usuario en dias
-edad_dias = edad * dias_año;
+edad_dias = edad * dias_año
 
 #Se imprime en pantalla la edad en dias
 print(f"Tienes actualmente '{edad}' años de edad, lo que equivale aproximadamente a '{edad_dias}' dias.")
